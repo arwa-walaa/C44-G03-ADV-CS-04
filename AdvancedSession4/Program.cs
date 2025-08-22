e87ef2c7e71efa389956cd7699f0b86c74bd5bf6
arwa-walaa <84721941+arwa-walaa@users.noreply.github.com>
2025-08-22 17:29:42 +0300

    Delegate & Delegate Example 01

--- AdvancedSession4/Program.cs
+++ AdvancedSession4/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Delegate 
+
+            #region Delegate Example 1
+
+            //create function take sting and return number of uppercae letters
+            StringFunctionDelegate refrence;
+            refrence = new StringFunctionDelegate(StringFunction.CountUppercase);
+            refrence = StringFunction.CountUppercase;
+
+            refrence += StringFunction.CountLowercase;
+            //refrence -= StringFunction.CountLowercase;
+
+            int result = refrence.Invoke("Hello World");
+            Console.WriteLine(result);
+
+            #endregion
+
+
+            #endregion
         }
     }
 }
