f8c9cc8dca5e135c5c5c21f3e3108ccc9e787ed7
arwa-walaa <84721941+arwa-walaa@users.noreply.github.com>
2025-08-23 14:27:15 +0300

    Using User Defined Delegate

---
+++ Assignment/LibraryEngine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    public delegate string BookFunctionDelegate(Book B);
+
+
+    public class LibraryEngine
+    {
+        // Using User Defined Delegate
+        public static void ProcessBooks(List<Book> blist, BookFunctionDelegate fPtr)
+        {
+            foreach (Book B in blist)
+            {
+                Console.WriteLine(fPtr.Invoke(B));
+            }
+        }
+
+      
+    }
+}
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create sample books
+            List<Book> books = new List<Book>
+        {
+            new Book("978-3-16-148410-0", "C# Programming",
+                    new string[] {"John Doe", "Jane Smith"},
+                    new DateTime(2023, 1, 15), 49.99m),
+            new Book("978-1-23-456789-7", "Advanced C#",
+                    new string[] {"Alice Johnson"},
+                    new DateTime(2024, 3, 20), 79.99m)
+        };
+            #region Using User Defined Delegate
+
+            Console.WriteLine("=== Using User Defined Delegate ===");
+            LibraryEngine.ProcessBooks(books, BookFunctions.GetTitle);
+            LibraryEngine.ProcessBooks(books, BookFunctions.GetAuthors);
+            LibraryEngine.ProcessBooks(books, BookFunctions.GetPrice);
+            #endregion
+
         }
     }
 }
